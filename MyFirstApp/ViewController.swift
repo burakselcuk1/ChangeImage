bd19e6e3e02fab28e5f921f57681b26266a0e7b3
burak <burak.selcuk@demirorenteknoloji.comburak.selcuk@demirorenteknoloji.comburak.selcuk@demirorenteknoloji.comburak.selcuk@demirorenteknoloji.com>
2022-10-30 22:52:46 +0300

    second commit

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var burakBabaLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func changeClick(_ sender: Any) {
+        imageView.image = UIImage(named:"dark_sepia_nature_background_210839")
+       // imageView.image = UIImage(named:"dark_sepia_nature_background_210839")
+
+    }
 }
 
